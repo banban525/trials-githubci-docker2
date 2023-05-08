2d53b4c27c62dac63f92874e1bb8164cc4462e58
banban525 <banban525@users.noreply.github.com>
2023-05-08 23:21:57 +0900

    prototype github action service

--- __integrationTests/__tests__/integrationTests.ts
+++ __integrationTests/__tests__/integrationTests.ts
@@ -1,13 +1,25 @@
 import mqtt, { IPublishPacket, ISubscriptionGrant } from "mqtt";
 import fetch from "node-fetch";
 
+
+let mqttServer = "mqtt://localhost"
+if("mqttserver" in process.env && process.env.mqttserver !== undefined)
+{
+  mqttServer = process.env.mqttserver;
+}
+let emurator = "http://localhost:3000"
+if("emurator" in process.env && process.env.emurator !== undefined)
+{
+  emurator = process.env.emurator;
+}
+
+
 test('integration test', () => {
   expect(1+1).toBe(2);
 });
 
-
 test("connet to mqtt", (done:jest.DoneCallback)=>{
-  const mqttBroker = "mqtt://localhost";
+  const mqttBroker = mqttServer;
   const mqttClient = mqtt.connect(mqttBroker);
   mqttClient.on("connect", ()=>{
     console.log("connected");
@@ -15,14 +27,14 @@ test("connet to mqtt", (done:jest.DoneCallback)=>{
     done();
   });
   
-}, 5*1000);
+});
 
 
 test("wait for emulator", async ():Promise<void>=>{
 
-  const res = await fetch("http://localhost:3000");
+  const res = await fetch(emurator);
   const text = await res.text();
   expect(text ).toContain("echonet-lite-kaden-emulator");
-}, 10*1000);
+});
 
 
